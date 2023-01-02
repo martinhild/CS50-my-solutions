51224dd37344ccb07f2a39f2d952a0fe5d481f16
martinhild <115345954+martinhild@users.noreply.github.com>
2023-01-02 14:20:55 +0000

    Mon, Jan 2, 2023, 2:20 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,19 +5,22 @@ int main(void)
 {
     int height;
     int width;
+    string line;
+
+    //Get user input:
     do
     {
         height = get_int("Enter height: ");
     }
     while (height < 1 || height > 8);
 
-
+    //Loop
     for (int i = 0; i < height; i++)
     {
         width=i+1;
-        for (int j = 0; j < 8 ; j++)
+        for (int j = 0; j < width ; j++)
         {
-            if(j<)
+
             printf("#");
         }
         printf("\n");
