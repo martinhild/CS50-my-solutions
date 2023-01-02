4eb3befd571b8071efd663d9a1b0b854186a9405
martinhild <115345954+martinhild@users.noreply.github.com>
2023-01-02 08:31:53 +0000

    Mon, Jan 2, 2023, 8:31 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-//#include <cs50.h>
+#include <cs50.h>
 
 int main(void)
 {
-    //string name = get_string("What's your name? ");
+    string name = get_string("What's your name?\n");
 
-    printf("hello, world\n");
+    printf("hello, %s\n",name);
 }
