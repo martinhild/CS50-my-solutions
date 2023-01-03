e38c527dfb5275b1e28e5891cf9dc05da3f42e48
martinhild <115345954+martinhild@users.noreply.github.com>
2023-01-03 08:35:59 +0000

    Tue, Jan 3, 2023, 8:35 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -14,12 +14,13 @@ int main(void)
     }
     while (height < 1 || height > 8);
 
-    //Loop through lines
+    //Loop from line '0' till line 'height'
     for (int i = 0; i < height; i++)
     {
-
+        //Loop from column '0' till column 'height'
         for (int j = 0; j < height ; j++)
         {
+            //decide if printing '.' or '#'
             if( j < height - i - 1 ){
                 printf(".");
             }
@@ -28,6 +29,7 @@ int main(void)
                 printf("#");
             }
         }
+        //print new line after one line is finished
         printf("\n");
     }
 }
