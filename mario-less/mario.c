fee912aeb347a2a1863076b2d5c560fd73695113
martinhild <115345954+martinhild@users.noreply.github.com>
2023-01-03 07:19:12 +0000

    Tue, Jan 3, 2023, 7:19 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -18,9 +18,9 @@ int main(void)
     for (int i = 0; i < height; i++)
     {
         width=i+1;
-        for (int j = 0; j < width ; j++)
+        for (int j = 0; j < height ; j++)
         {
-            if( j < i ){
+            if( j > i ){
                 printf(".");
             }
             else
