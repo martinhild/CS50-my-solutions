49501491da92be407c5106d6cc5892062b1f5fad
martinhild <115345954+martinhild@users.noreply.github.com>
2023-01-18 18:51:46 +0000

    Wed, Jan 18, 2023, 6:51 PM +00:00

--- to-binary/binary.c
+++ to-binary/binary.c
@@ -6,9 +6,10 @@ int main(void)
 {
     int decimal = get_int("Integer: ");
 
-    int binary_array[8];
+    int size = 8;
+    int binary_array[size];
 
-    int i;
+    int i = 0;
     while(decimal > 0)
     {
         binary_array[i] = decimal % 2;
