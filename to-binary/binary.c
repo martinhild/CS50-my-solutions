0582f7d69b7899b045cbc0fb2d6b348d5073c4ec
martinhild <115345954+martinhild@users.noreply.github.com>
2023-01-18 17:19:40 +0000

    Wed, Jan 18, 2023, 5:19 PM +00:00

---
+++ to-binary/binary.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int decimal = get_int("Integer: ");
+
+
+    printf("%i\n", decimal);
+
+    int binary_array
+}
