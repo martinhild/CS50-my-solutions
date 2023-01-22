d4cbae405515774439eac39f2eccddde62db4e8e
martinhild <115345954+martinhild@users.noreply.github.com>
2023-01-22 13:57:47 +0000

    Sun, Jan 22, 2023, 1:57 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -14,7 +14,7 @@ int main(void)
 
 //get Text
 //string text = get_string("Text: ");
-string text = "Would you like them here or there? I would not like them here or there. I would not like them anywhere.";
+string text = "One fish. Two fish. Red fish. Blue fish.";
 //print number of letters, words, sentences
 //printf("%i letters\n", count_letters(text));
 //printf("%i words\n", count_words(text));
