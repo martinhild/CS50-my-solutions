2092e6dd605ba95a521ea8711c650462fdb9a887
martinhild <115345954+martinhild@users.noreply.github.com>
2023-01-22 13:13:50 +0000

    Sun, Jan 22, 2023, 1:13 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -53,7 +53,7 @@ int count_sentences(string text)
     int sentences = 0;
     for (int i = 0; i < strlen(text); i++)
     {
-        if (text[i] == '!')
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
         {
             sentences++;
         }
