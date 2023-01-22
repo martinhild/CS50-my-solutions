cb6fe8fa64c00422d62a606f5a3e3b3e14aa4df0
martinhild <115345954+martinhild@users.noreply.github.com>
2023-01-22 13:45:05 +0000

    Sun, Jan 22, 2023, 1:45 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -13,7 +13,8 @@ int main(void)
 {
 
 //get Text
-string text = get_string("Text: ");
+//string text = get_string("Text: ");
+string text = "Would you like them here or there? I would not like them here or there. I would not like them anywhere.";
 //print number of letters, words, sentences
 //printf("%i letters\n", count_letters(text));
 //printf("%i words\n", count_words(text));
@@ -31,6 +32,7 @@ float index = 0.0588 * l - 0.296 * s - 15.8;
 // index auf nächsten integer runden
 int grade = rint(index);
 
+if grade 
 printf("%i\n", grade);
 
 }
