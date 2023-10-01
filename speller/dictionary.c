b04eb5f4764a45aa13c444d1109d4051c37faf06
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-01 13:56:27 +0000

    Sun, Oct 1, 2023, 3:56 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -26,7 +26,7 @@ node;
 const unsigned int N = 26;
 
 // Hash table
-node *table[N];
+node *table[N];  //malloc?????
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
