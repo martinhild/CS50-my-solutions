47715e3250c5bf92ac59b26a11e4ea788b4b7293
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-02 14:25:55 +0000

    Mon, Oct 2, 2023, 4:25 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,8 +1,12 @@
 # TODO
-# from cs50 import get_int
+from cs50 import get_int
 # from cs50 import get_string
 
 def main():
-    print("hi")
+    number = get_number()
+    print(number)
+
+def get_number():
+    return get_int("Number: ")
 
 main()
