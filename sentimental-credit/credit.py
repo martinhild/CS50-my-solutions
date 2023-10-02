2f9fc2dff9a1e716d94827db13fe5ba91fb53fdb
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-02 15:09:29 +0000

    Mon, Oct 2, 2023, 5:09 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,8 +4,8 @@ from cs50 import get_string
 
 
 def main():
-    #number = get_number()
-    number = list[1,2,3,4,5,6,7,8,9]
+    number = get_number()
+
     number.reverse()
     for x in number[::2]:
         print(x)
