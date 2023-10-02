ff0debecf75f8881a0068641b4d5ff70f1d343af
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-02 12:13:36 +0000

    Mon, Oct 2, 2023, 2:13 PM +02:00

--- sandbox/buggy2.c
+++ sandbox/buggy2.c
@@ -1,10 +1,11 @@
+//Buggy example for printfand debug50
+
 #include <stdio.h>
 
 int main(void)
 {
     for (int i = 0; i <= 10; i++)
     {
-        printf("i is now %i: ", i);
         printf("#\n");
     }
 }
