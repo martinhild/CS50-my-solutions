edf4cf143fabc7f50fa59b960a390eb0bcdc7c7c
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-02 13:49:25 +0000

    Mon, Oct 2, 2023, 3:49 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,10 +1,7 @@
 # TODO
 def main():
-
-    # for row in range(height()):
-    #     print(row)
-
-    print(2 * get_height())
+    height = get_height
+    print(height)
 
 #get height from user input
 def get_height():
