888f34cb2d8baffb68da3c77b13c795b0d90a904
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-02 15:57:26 +0000

    Mon, Oct 2, 2023, 5:57 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -11,20 +11,16 @@ def main():
     string_1 = ""
     for x in number[len(number)-2 :: -2]:
         string_1 += str((int(x) * 2))
-    print(string_1)
 
     # sum of string_1's digits
-    sum_1 = 0
+    sum = 0
     for x in string_1:
-        sum_1 += int(x)
+        sum += int(x)
 
-    # sum up the other digits of the user's number
-    sum_2 = 0
+    # add the other digits of the user's number to sum
     for x in number[len(number)-1 :: -2]:
-        sum_2 += int(x)
+        sum += int(x)
 
-    # add sums together
-    sum = sum_1 + sum_2
     print(sum)
 
 
