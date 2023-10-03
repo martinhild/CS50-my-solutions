d68a8eb6f0af786cab2399e329636214b58edfe4
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-03 10:00:34 +0000

    Tue, Oct 3, 2023, 12:00 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-
+    len(sys.argv)
     # TODO: Read database file into a variable
     file = open(,"a")
     # TODO: Read DNA sequence file into a variable
