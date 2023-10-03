c321266789d9363129440363a9c3d80475f639fd
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-03 08:40:43 +0000

    Tue, Oct 3, 2023, 10:40 AM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,23 +4,23 @@ from cs50 import get_string
 
 def main():
     text = get_string("Text: ")
-    letters = letters(text)
-    words = words(text)
+    letters = count_letters(text)
+    words = count_words(text)
     # sentences = sentences(text)
 
-    L = 100 * letters(text) / words(text)
+    L = 100 * letters / words
 
     print(letters)
     print(words)
 
-def letters(text):
+def count_letters(text):
     letters = 0
     for c in text:
         if "A" <= c <= "Z" or "a" <= c <= "z":
             letters += 1
     return letters
 
-def words(text):
+def count_words(text):
     words = 0
     for c in text:
         if c == " ":
