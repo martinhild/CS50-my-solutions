997c19f83007b9b38a5ebea8993e180df3b83b43
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-03 07:23:14 +0000

    Tue, Oct 3, 2023, 9:23 AM +02:00

--- sandbox/test.py
+++ sandbox/test.py
@@ -1,30 +1,4 @@
-def main():
-    LuhnsAlgorithm("4062901840")
+x = input("Number: ")
 
-# check if a credit card number is valid
-def LuhnsAlgorithm(number):
-    # add every other digit multiplied by 2 to a string, starting with the number’s second-to-last digit,
-    string_1 = ""
-    for x in number[len(number) - 2 :: -2]:
-        string_1 += str((int(x) * 2))
-
-    print(string_1)
-
-    # sum of string_1's digits
-    sum = 0
-    for x in string_1:
-        sum += int(x)
-    print(sum)
-
-    # add the other digits of the user's number to sum
-    for x in number[len(number) - 1 :: -2]:
-        sum += int(x)
-
-    print(sum)
-    # check if sum's last digit is 0
-    if sum % 10 == 0:
-        return True
-    else:
-        return False
-
-main()
+if x == 3 or 4:
+    print("yes")
