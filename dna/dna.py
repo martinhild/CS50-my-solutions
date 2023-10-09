53ad9d91a3ce6f87469ec73aad42159992741c95
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-09 12:58:33 +0000

    Mon, Oct 9, 2023, 2:58 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -15,7 +15,8 @@ def main():
 
         # TODO: Read DNA sequence file into a variable
         with open(sys.argv[2],"r") as sequence:
-            
+            print(sequence)
+
     # TODO: Find longest match of each STR in DNA sequence
 
     # TODO: Check database for matching profiles
