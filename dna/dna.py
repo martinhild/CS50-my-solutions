95e324ac767b27c958a96f2983f88813f7c4e3f5
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-09 13:48:20 +0000

    Mon, Oct 9, 2023, 3:48 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -20,7 +20,7 @@ def main():
 
     # TODO: Find longest match of each STR in DNA sequence
 
-    for i in 4:
+    for i in range(len(reader.fieldnames)):
         print(reader.fieldnames[i])
     # TODO: Check database for matching profiles
 
