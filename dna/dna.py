c62eb374778da47017c0d3689dbb4f74ae4cbb61
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-09 13:10:09 +0000

    Mon, Oct 9, 2023, 3:10 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -10,8 +10,8 @@ def main():
         return 0
 
     # TODO: Read database file into a variable
-    database = open(sys.argv[1],"r")
-    reader = csv.DictReader(database)
+    #database = open(sys.argv[1],"r")
+    #reader = csv.DictReader(database)
 
     # TODO: Read DNA sequence file into a variable
     sequence = open(sys.argv[2],"r")
