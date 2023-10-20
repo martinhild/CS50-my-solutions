3066e88f7349e546223e8a1dafe8c2f2d2bfd24e
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 07:09:44 +0000

    Fri, Oct 20, 2023, 9:09 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,5 @@
-SELECT movie_id FROM stars WHERE people_id = (
+
+
+SELECT movie_id FROM stars WHERE person_id = (
     SELECT id from people WHERE name = "Kevin Bacon" AND birth = "1958"
-    );
+    ) l;
