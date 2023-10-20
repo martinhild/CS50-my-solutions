9e8980bb672c5f97f37bfa312f5372a9c7d57130
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 12:46:33 +0000

    Fri, Oct 20, 2023, 2:46 PM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -4,4 +4,5 @@ ON people.id = stars.person_id AND stars.movie_id = movies.id
 AND movies.id = ratings.movie_id
 WHERE people.name = "Chadwick Boseman"
 ORDER BY ratings.rating DESC
-LIMIT 5;
+LIMIT 5
+;
