90be19b3cc8c950903b875580f4294d3007d3c30
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 15:44:35 +0000

    Fri, Oct 20, 2023, 5:44 PM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,6 @@
-SELECT  people.name
+SELECT people.name
 FROM people JOIN stars JOIN movies
 ON people.id = stars.person_id AND stars.movie_id = movies.id
 WHERE movies.year = "2004"
+AND
 ORDER BY people.birth;
