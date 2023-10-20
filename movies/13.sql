fad71538fa34c5c917288147e159fb0e135e694c
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 06:22:13 +0000

    Fri, Oct 20, 2023, 8:22 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,5 @@
 SELECT people.name
 FROM people JOIN stars JOIN movies
 ON people.id = stars.person_id AND stars.movie_id = movies.id
-WHERE people.name = "Kevin Bacon";
+WHERE people.name = "Kevin Bacon"
+LIMIT 10;
