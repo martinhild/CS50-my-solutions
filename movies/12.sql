328e7848ecc2713722b6ca82512f61a74ae3f714
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 10:06:43 +0000

    Fri, Oct 20, 2023, 12:06 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -4,7 +4,7 @@ ON people.id = stars.person_id AND stars.movie_id = movies.id
 WHERE
 
 movies.id = (SELECT stars.person_id = (SELECT id FROM people WHERE name = "Bradley Cooper"))
-AND
-movies.id = (SELECT stars.person_id = (SELECT id FROM people WHERE name = "Jennifer Lawrence"))
+-- AND
+-- movies.id = (SELECT stars.person_id = (SELECT id FROM people WHERE name = "Jennifer Lawrence"))
 
 ORDER BY movies.title;
