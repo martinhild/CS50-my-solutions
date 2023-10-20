45d4f4b345dd6c320521de7fcc0f163bf59d2d07
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 12:40:43 +0000

    Fri, Oct 20, 2023, 2:40 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT movies.title
+SELECT movies.title
 FROM people JOIN stars JOIN movies
 ON people.id = stars.person_id AND stars.movie_id = movies.id
 WHERE
@@ -7,7 +7,6 @@ WHERE
     OR
     people.name = "Bradley Cooper"
     )
-
---GROUP BY movies.title
---HAVING COUNT(*) = 2
+GROUP BY movies.title
+HAVING COUNT(*) = 2
 ;
