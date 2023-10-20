62e5c3e11c963c987d1162c53806b9d3db4f43e6
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 06:52:03 +0000

    Fri, Oct 20, 2023, 8:52 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -10,6 +10,8 @@
 
     -- )
 LIMIT 10;
-
-SELECT person_id FROM stars WHERE movie_id = "83833";
+SELECT name FROM people WHERE id =
+    (
+    SELECT person_id FROM stars WHERE movie_id = "83833";
+    )
 SELECT title FROM movies WHERE id = "83833";
