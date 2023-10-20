f7d31a30c6b252ee48acb97c9d1e66da5c163f45
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 10:15:31 +0000

    Fri, Oct 20, 2023, 12:15 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -3,7 +3,7 @@ FROM people JOIN stars JOIN movies
 ON people.id = stars.person_id AND stars.movie_id = movies.id
 WHERE
 
-movies.id = (SELECT stars.person_id = (SELECT id FROM people WHERE name = "Bradley Cooper"))
+movies.id = (SELECT stars.movie_id = (SELECT id FROM people WHERE name = "Bradley Cooper"))
 -- AND
 -- movies.id = (SELECT stars.person_id = (SELECT id FROM people WHERE name = "Jennifer Lawrence"))
 
