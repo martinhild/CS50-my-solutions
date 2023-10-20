22d16eb7a63787679e5718adebf53b6613241072
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 11:39:31 +0000

    Fri, Oct 20, 2023, 1:39 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,10 +1,6 @@
-
 SELECT movies.title, COUNT(*)
 FROM people JOIN stars JOIN movies
 ON people.id = stars.person_id AND stars.movie_id = movies.id
-
-
-
 
 WHERE
 
@@ -12,10 +8,6 @@ people.name = "Jennifer Lawrence"
 OR
 people.name = "Bradley Cooper"
 
-AND
-
-COUNT(*) = 2
-
 GROUP BY movies.title
 
-;
+ORDER BY movies.title;
