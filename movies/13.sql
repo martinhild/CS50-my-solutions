1debcfafd493ef542598dd5a93d5361b30589146
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 06:45:32 +0000

    Fri, Oct 20, 2023, 8:45 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,10 +1,13 @@
 -- SELECT name FROM people WHERE id =
+--     (
+--     SELECT person_id FROM stars WHERE movie_id =
+--         (
+        SELECT movie_id FROM stars WHERE person_id =
+            (
+            SELECT id FROM people
+            WHERE people.name = "Kevin Bacon"
+            )
+    --     )
 
-SELECT person_id FROM stars WHERE movie_id =
-    (SELECT movie_id FROM stars WHERE person_id =
-        (SELECT id FROM people
-        WHERE people.name = "Kevin Bacon"
-        )
-    )
-
+    -- )
 LIMIT 10;
