d806ff4f56282da74d42ba79080fc4f9c91808b1
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 06:59:43 +0000

    Fri, Oct 20, 2023, 8:59 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,6 @@
-SELECT name FROM people WHERE id =
-    (SELECT person_id FROM stars WHERE movie_id = "83833";
-    );
+-- SELECT name FROM people WHERE id = (
+    SELECT person_id FROM stars WHERE movie_id = "83833"
+    -- )
+    ;
+
+SELECT name FROM people WHERE id = "620";
