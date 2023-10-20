5c4b5de767f5c851d613bfe3f8a155645963b592
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 16:48:42 +0000

    Fri, Oct 20, 2023, 6:48 PM +02:00

--- movies/test.sql
+++ movies/test.sql
@@ -1,6 +1,9 @@
 
-SELECT person_id FROM directors WHERE 
-SELECT movie_id FROM ratings WHERE rating >= "9.0"
-
-
+SELECT name FROM people WHERE id IN
+    (
+    SELECT person_id FROM directors WHERE movie_id IN
+        (
+        SELECT movie_id FROM ratings WHERE rating >= "9.0"
+        )
+    )
 ;
