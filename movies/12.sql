4e0e78eaf9a1824974cab3e7c40baa4c287b888e
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-20 07:51:36 +0000

    Fri, Oct 20, 2023, 9:51 AM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,6 +1,6 @@
 SELECT movies.title, people.name
 FROM people JOIN stars JOIN movies
 ON people.id = stars.person_id AND stars.movie_id = movies.id
-WHERE people.id = (SELECT id FROM people WHERE name = ("Bradley Cooper"))
+WHERE movie.id = (SELECT starsid FROM people WHERE name = ("Bradley Cooper"))
 
 ORDER BY movies.title;
