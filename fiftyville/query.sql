0e5a5a2debdceedf7dc61ac62bf5fc26ef53bb08
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-21 12:16:05 +0000

    Sat, Oct 21, 2023, 2:16 PM +02:00

--- fiftyville/query.sql
+++ fiftyville/query.sql
@@ -3,6 +3,10 @@
 
 SELECT *
   FROM crime_scene_reports
+ WHERE description = "%duck%";
+
+ SELECT *
+  FROM crime_scene_reports
  WHERE month = 7
    AND day = 28
    AND street = "Humphrey Street";
