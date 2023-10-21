7d0b7f06161dfa1411edaaa2d44425d9a459fa13
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-21 07:14:25 +0000

    Sat, Oct 21, 2023, 9:14 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 SELECT (*)
-  FROM crime_scene_reports
-
-  sqlite> SELECT (*)
-   ...>   FROM crime_scene_reports
+  FROM crime_scene_reports;
