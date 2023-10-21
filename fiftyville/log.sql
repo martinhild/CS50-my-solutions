e17fb6fb855f2b18f23e34f572ed7d1f52fc632b
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-21 14:59:24 +0000

    Sat, Oct 21, 2023, 4:59 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -135,3 +135,5 @@ SELECT *
 SELECT origin_airport_id, destination_airport_id
   FROM flights
  WHERE id = 36;
+
+-- Airports:
--- fiftyville/query.sql
+++ fiftyville/query.sql
@@ -1,3 +1,3 @@
-SELECT origin_airport_id, destination_airport_id
-  FROM flights
- WHERE id = 36;
+SELECT *
+  FROM airports
+ WHERE id = 8;
