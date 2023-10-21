f5aa9d2debec5f8351f14aeddb5f5bbb6fac3052
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-21 13:17:04 +0000

    Sat, Oct 21, 2023, 3:17 PM +02:00

--- fiftyville/query.sql
+++ fiftyville/query.sql
@@ -1,4 +1,10 @@
--- search for drivers and check ATM activity
-SELECT *
-  FROM atm_transactions
-  LIMIT 10;
+-- -- search for drivers and check ATM activity
+-- SELECT *
+--   FROM atm_transactions
+--   LIMIT 10;
+
+  SELECT *
+  FROM interviews
+ WHERE month = 7
+   AND day = 28
+   AND transcript LIKE "%bakery%";
