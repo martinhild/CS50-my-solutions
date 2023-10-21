54f520bc475b573ced57f77c4760bad9d82bceec
martinhild <115345954+martinhild@users.noreply.github.com>
2023-10-21 13:09:24 +0000

    Sat, Oct 21, 2023, 3:09 PM +02:00

--- fiftyville/query.sql
+++ fiftyville/query.sql
@@ -1,12 +1,10 @@
-
---cat query.sql | sqlite3 fiftyville.db
+--cars that left within 10min of 10:15am = 10:10 - 10:20
 
 SELECT *
   FROM bakery_security_logs
  WHERE day = 28
    AND hour = 10
-   AND minute >= 5
-   AND minute >= 5
+   AND minute >= 10
    AND minute <= 20
 LIMIT 10;
 --  WHERE month = 7
