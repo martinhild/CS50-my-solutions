3e45455e3b638f350f298abd192728edb5cfc6df
martinhild <115345954+martinhild@users.noreply.github.com>
2024-02-08 13:12:58 +0000

    Thu, Feb 8, 2024, 2:12 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -125,7 +125,7 @@ def register():
         if password == "" or confirmation == "":
             return apology("enter a password and confirm it")
         if password != confirmation:
-            return apology("enter a password and confirm it")
+            return apology("password and confirmation did not match")
     else:
         return render_template("register.html")
 
