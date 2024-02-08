4bede94290fade81643ec3f79ba49956f2a6d046
martinhild <115345954+martinhild@users.noreply.github.com>
2024-02-08 12:51:24 +0000

    Thu, Feb 8, 2024, 1:51 PM +01:00

--- sandbox/testing/test.py
+++ sandbox/testing/test.py
@@ -2,7 +2,7 @@ from cs50 import SQL
 
 db = SQL("sqlite:///finance.db")
 
-rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+rows = db.execute("SELECT * FROM users WHERE username = ?", "username")
 
-print(rows)
+print(len(rows))
 
