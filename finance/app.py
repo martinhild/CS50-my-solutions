59cff65c01f659bfe30a418eaed776b4017f528b
martinhild <115345954+martinhild@users.noreply.github.com>
2024-02-08 12:06:54 +0000

    Thu, Feb 8, 2024, 1:06 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -113,8 +113,7 @@ def quote():
 def register():
     """Register user"""
     username = request.form.get("username"):
-        db.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        db.execute("SELECT * FROM users WHERE username = ?", request.form.get(username))
     else:
         return apology("TODO")
 
