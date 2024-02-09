b127badee7ba6102c73fdacf511b8b3b8af8f15a
martinhild <115345954+martinhild@users.noreply.github.com>
2024-02-09 09:02:19 +0000

    Fri, Feb 9, 2024, 10:02 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -106,10 +106,10 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    if request.method == "POST":
+    if request.method == "GET":
+        return render_template("quote.html")
 
 
-        
     return apology("TODO")
 
 
