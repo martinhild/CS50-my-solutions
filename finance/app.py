463d00629565d5750615baadf623e91c0ee1b565
martinhild <115345954+martinhild@users.noreply.github.com>
2024-02-09 17:35:18 +0000

    Fri, Feb 9, 2024, 6:35 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -77,7 +77,7 @@ def buy():
                 # Subtract the cost of the transaction from user's cash in users table
                 cash = db.execute("SELECT cash FROM users WHERE id = ?", userid)[0]["cash"]
                 cash = cash - cost
-                db.execute("UPDATE users cash FROM users WHERE id = ?", userid)[0]["cash"]
+                db.execute("UPDATE users SET cash = ? WHERE id = ?",cash ,userid)
 
                 ###################################################
                 #####################################################
