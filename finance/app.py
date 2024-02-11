76f9c0ca1a15ca489bd82a7f983f0eca39a47ebc
martinhild <115345954+martinhild@users.noreply.github.com>
2024-02-11 18:14:12 +0000

    Sun, Feb 11, 2024, 7:14 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -227,11 +227,7 @@ def sell():
         username = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])[0]["username"]
 
         owned = db.execute("SELECT amount FROM stocks WHERE  username = ? AND symbol = ?", username, symbol)[0]["amount"]
-        print("HERE")
-        print("HERE")
-        print("HERE")
-        print(symbol)
-        return apology(owned)
+        return apology(symbol)
         if owned <= 500:
             return apology("You don't own any shares of that stock")
 
