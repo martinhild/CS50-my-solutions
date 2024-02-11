bbfe3059c4d12c2789ad47ec7a0a77fb83a58050
martinhild <115345954+martinhild@users.noreply.github.com>
2024-02-11 12:02:17 +0000

    Sun, Feb 11, 2024, 1:02 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -88,10 +88,8 @@ def buy():
                 db.execute("UPDATE users SET cash = ? WHERE id = ?", cash, userid)
 
                 # Save the transaction into 'transactions' table
-                db.execute(
-                    "INSERT INTO transactions (username, action, symbol, price, amount, datetime)
-                    VALUES (?, ?, ?, ?, ?, ?)", username, action, symbol, price, amount, t
-                )
+                db.execute("INSERT INTO transactions(username, action, symbol, price, amount, datetime)
+                           VALUES(?, ?, ?, ?, ?, ?)", username, action, symbol, price, amount, t)
 
                 # Save the transaction into 'stocks' table
                 if db.execute("SELECT * FROM stocks WHERE username = ? AND symbol = ?", username, symbol)
