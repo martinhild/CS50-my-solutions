b53bb3537cd01855e3a9b92cb08163e4162054dc
martinhild <115345954+martinhild@users.noreply.github.com>
2024-04-27 14:18:54 +0000

    Sat, Apr 27, 2024, 4:18 PM +02:00

---
+++ project/OptionsActivity.java
@@ -0,0 +1,59 @@
+package com.example.sqltest;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.res.Resources;
+import android.widget.Spinner;
+import java.util.Locale;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
+    public final String[] languages = {"Select Language", "English", "German"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedLang = parent.getItemAtPosition(position).toString();
+
+                if (selectedLang.equals("English")) {
+                    setLocal(OptionsActivity.this, "en");
+                    finish();
+                    startActivity(getIntent());
+                } else if (selectedLang.equals("German")) {
+                    setLocal(OptionsActivity.this, "de");
+                    finish();
+                    startActivity(getIntent());
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    public void setLocal(Activity activity, String langCode) {
+        Locale locale = new Locale(langCode);
+        locale.setDefault(locale);
+        Resources resources = activity.getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+    }
+}
